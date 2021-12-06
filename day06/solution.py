d55c52fd7a4979074cbf1e38fbbd441cebd10d7e
aleksandar-had <aleksandar@hey.com>
2021-12-06 16:57:47 +0100

    day06: solve Lanternfish problem with different approaches

---
+++ day06/solution.py
@@ -0,0 +1,61 @@
+# For fun, tracked time difference between the 2 approaches
+import functools
+import time
+
+def timer(function):
+    @functools.wraps(function)
+    def wrapper_timer(*args, **kwargs):
+        start = time.perf_counter()
+        _ = function(*args, **kwargs)
+        finish = time.perf_counter()
+        print(f"Completed in {finish - start:0.5f} secs")
+        return _
+    return wrapper_timer
+
+##
+# Solution begins here
+##
+def parse_input():
+    res = []
+    for line in open("input.txt"):
+        res.extend([int(timer) for timer in line.split(',')]) 
+    return res
+
+@timer
+def solution_part1(days):
+    fish = parse_input()    
+        
+    for day in range(days):
+        for i in range(len(fish)):
+            if fish[i] == 0:
+                fish.append(8)
+                fish[i] = 6
+            else:
+                fish[i] -= 1
+        
+    print(len(fish))
+    
+# Solution to part1 was very brute-force (tracking each individual fish)
+# Instead track the number of fish in each individual day
+@timer
+def solution_part2(days):
+    fish = parse_input()
+    
+    fish_at_day = [0]*9
+    
+    for day in fish:
+        fish_at_day[day] += 1
+    
+    for _ in range(days):
+        fish_at_zero = fish_at_day[0]
+        for fish_day in range(8):
+            fish_at_day[fish_day] = fish_at_day[fish_day + 1]
+        fish_at_day[6] += fish_at_zero
+        fish_at_day[8] = fish_at_zero
+    
+    print(sum(fish_at_day))
+    
+    
+if __name__ == "__main__":
+    solution_part1(80)
+    solution_part2(256)
